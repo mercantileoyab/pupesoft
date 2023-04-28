3c81cb3291e4282ec959601eb1d47ba49c3c08fd
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2023-04-28 11:10:25 +0300

    Publishing price update feature

--- import_saldo_ostohinta.php
+++ import_saldo_ostohinta.php
@@ -1033,7 +1033,7 @@ class ImportSaldoHinta
           $loydetyt_tuotteet[] = $rivi;
           $onnistunut_tuote = true;
 
-          if($tuotesaldo > 0 and 1==2) {
+          if($tuotesaldo > 0) {
 
             $toimittajat_idt = implode(",", array_values($this->toimittajat_tiedostot));
 
